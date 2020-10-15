d82c9d6a10f11755281f88a1861cd2abd44bfa25
HoVietHoang <61863979+nhom1-hoang-dinh@users.noreply.github.com>
2020-10-15 13:33:40 +0700

    Add files via upload

---
+++ database/migrations/2020_10_15_062010_create_table_product_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->id();
+            $table->string('name_product');
+            $table->int('price');
+            $table->int('id_type');
+            $table->string('content');
+            $table->int('status');
+            $table->int('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product');
+    }
+}
